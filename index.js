7a495691ddb461b1f16f86e0bf3c1d520e56d046
Justin McCraw <jmccraw@ufl.edu>
2022-05-22 21:25:27 -0400

    completed instructions 1-4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+
+function greet() {
+    var username = prompt('What is your name?') 
+    
+    if(username) {
+        alert("Hello, " + username)
+    } else {
+        alert('Hello')
+    }
+
+    var userAge = prompt('How old are you?')
+        parseInt(userAge)
+
+    var findAge = confirm('Click ok if you had a birthday so far this year')
+
+    if (findAge) {
+        alert("You must have been born in " + userAge, new Date().getFullYear())
+    } else {
+        alert("You must have been born in " + userAge + 1 + new Date().getFullYear())
+    }
+
+
+}
